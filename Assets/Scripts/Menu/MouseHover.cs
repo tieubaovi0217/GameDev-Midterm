e1b5006a6a4df1f138def7d89530b15f1fc109f9
Ky <heovang097@gmail.com>
2021-11-20 11:37:43 +0700

    Menu

--- Assets/Scripts/Menu/MouseHover.cs
+++ Assets/Scripts/Menu/MouseHover.cs
@@ -1,53 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MouseHover : MonoBehaviour
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
-    public Sprite orginalSprite;
+    public Sprite originalSprite;
     public Sprite onHoverSprite;
     private Image buttonImage;
-    private Transform buttonRectTransform;
+    private RectTransform buttonRectTransform;
     // Start is called before the first frame update
     void Start()
     {
         buttonImage = gameObject.GetComponent<Button>().image;
-        buttonRectTransform = gameObject.GetComponent<Button>().transform;
-        buttonImage.sprite = orginalSprite;
-    }
-
-    private void OnMouseOver()
-    {
-        buttonImage.sprite = onHoverSprite;
-        Debug.Log("On Mouse Over");
-    }
-
-    private void OnMouseExit()
-    {
-        Debug.Log("On Mouse Exit");
-        buttonImage.sprite = orginalSprite;
-    }
-
-    private void OnMouseEnter()
-    {
-
-        Debug.Log("On Mouse Enter");
-    }
-
-    private void OnMouseDown()
-    {
-
-    }
-
-    private void OnMouseUp()
-    {
-        
+        buttonRectTransform = gameObject.GetComponent<RectTransform>();
+        buttonImage.sprite = originalSprite;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.sprite = onHoverSprite;
+        buttonRectTransform.localScale = new Vector3(1.15f, 1.15f, 1.15f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonImage.sprite = originalSprite;
+        buttonRectTransform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
     }
 }
